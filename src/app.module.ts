99f0a19ae9942bd5d9336015c60d8cccd4f5adf4
¡Duque <atanaelleonardo@gmail.com>
2024-05-20 19:52:02 -0300

    "Removed AppController, AppService, and associated tests; added @nestjs/serve-static module and configured it to serve static files from 'public/404' directory."

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
+
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
-  exports: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public', '404'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class AppModule {}
