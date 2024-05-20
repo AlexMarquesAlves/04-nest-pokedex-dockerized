3b5a8f400f19f39e4085e07be8b04cabb6cdb642
¡Duque <atanaelleonardo@gmail.com>
2024-05-20 18:08:02 -0300

    "Added ConfigModule to AppModule, enabled CORS, and configured ValidationPipe in main.ts"

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const PORT = process.env.PORT
+
+  app.enableCors()
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
+  await app.listen(PORT, () => {
+    console.log(
+      `🚀 Running API server in MODE:${process.env.NODE_ENV} on Port:${PORT}`,
+    )
+  })
 }
 bootstrap()
