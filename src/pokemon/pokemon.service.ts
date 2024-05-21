9848d9ea13b5df3bc3c3d409947cd3e01d2f7997
¡Duque <atanaelleonardo@gmail.com>
2024-05-21 02:04:14 -0300

    "Removed docker-compose.yml file and updated PokemonService to use Mongoose model"

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,28 +1,38 @@
 import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
 import { CreatePokemonDto } from './dtos/create-pokemon.dto'
 import { UpdatePokemonDto } from './dtos/update-pokemon.dto'
+import { Pokemon } from './entities/pokemon.entity'
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase()
 
-    return createPokemonDto
+    const pokemon = await this.pokemonModel.create(createPokemonDto)
+
+    return pokemon
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all pokemon`
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} pokemon`
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+  async update(id: number, updatePokemonDto: UpdatePokemonDto) {
     return `This action updates a #${id} pokemon`
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} pokemon`
   }
 }
