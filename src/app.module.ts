cda6cec3e2d64771b5be4fca9925289055a78dcb
¡Duque <atanaelleonardo@gmail.com>
2024-05-20 23:27:17 -0300

    "Added 'pokemon' to vscode settings and imported MongooseModule in app.module.ts"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { join } from 'path'
 import { PokemonModule } from './pokemon/pokemon.module'
@@ -10,6 +11,7 @@ import { PokemonModule } from './pokemon/pokemon.module'
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public', '404'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
   ],
   controllers: [],
