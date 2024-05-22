e83fdaa00bb2424c1d8449369c6e14d435073fb0
¡Duque <atanaelleonardo@gmail.com>
2024-05-22 13:05:21 -0300

    "Removed CreateSeedDto and UpdateSeedDto, and reformatted code in Seed module and its components"

--- src/seed/seed.controller.spec.ts
+++ src/seed/seed.controller.spec.ts
@@ -1,20 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SeedController } from './seed.controller';
-import { SeedService } from './seed.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { SeedController } from './seed.controller'
+import { SeedService } from './seed.service'
 
 describe('SeedController', () => {
-  let controller: SeedController;
+  let controller: SeedController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SeedController],
       providers: [SeedService],
-    }).compile();
+    }).compile()
 
-    controller = module.get<SeedController>(SeedController);
-  });
+    controller = module.get<SeedController>(SeedController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
