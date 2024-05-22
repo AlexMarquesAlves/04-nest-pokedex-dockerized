0106d05ddf43568382e22c1125f345801f1371fb
¡Duque <atanaelleonardo@gmail.com>
2024-05-22 18:51:57 -0300

    "Remove seed dtos, entity, and CRUD operations; refactor seed controller and service to have a single executeSeed method"

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,42 +1,12 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common'
-import { CreateSeedDto } from './dtos/create-seed.dto'
-import { UpdateSeedDto } from './dtos/update-seed.dto'
+import { Controller, Get } from '@nestjs/common'
 import { SeedService } from './seed.service'
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
-  @Post()
-  create(@Body() createSeedDto: CreateSeedDto) {
-    return this.seedService.create(createSeedDto)
-  }
-
   @Get()
-  findAll() {
-    return this.seedService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.seedService.findOne(+id)
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSeedDto: UpdateSeedDto) {
-    return this.seedService.update(+id, updateSeedDto)
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.seedService.remove(+id)
+  async executeSeed() {
+    return this.seedService.executeSeed()
   }
 }
