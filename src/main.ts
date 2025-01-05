066a6106c1bf392d9e53f6264d7ea61943b3eacd
¡Duque <atanaelleonardo@gmail.com>
2025-01-05 14:24:16 -0300

    `Update environment variables and scripts in NestJS project`

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
-  app.setGlobalPrefix('api/v2');
+  app.setGlobalPrefix('api/v2')
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,12 +14,11 @@ async function bootstrap() {
       transform: true,
       transformOptions: {
         enableImplicitConversion: true,
-      }
-    })
-  );
+      },
+    }),
+  )
 
-
-  await app.listen( process.env.PORT );
-  console.log(`App running on port ${ process.env.PORT }`)
+  await app.listen(process.env.PORT)
+  console.log(`App running on port ${process.env.PORT}`)
 }
-bootstrap();
+bootstrap()
